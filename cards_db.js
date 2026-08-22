49909072aa9e8a14c86c18090b2f73e965cee37e
jongamer117-ops <jon.gamer.117@gmail.com>
2026-08-22 03:03:37 -0700

    release: cards_db.js

---
+++ cards_db.js
@@ -0,0 +1,21 @@
+const CardsDB = {
+    MUTACIONES: [
+        { id: "boss_apo_10", type: "MUTACIÓN", rarity: "SR", name: "Fragmento de la Primera Calamidad", era: "APOCALIPSIS", desc: "[ASIMILACIÓN] +100 DEF." }
+    ],
+    COMPANEROS: [
+        { id: "char_2b", type: "COMPAÑERO", rarity: "SSR", name: "2B (YoRHa)", era: "UNIVERSAL", desc: "+50 ATK, +50 DEF." }
+    ],
+    MASCOTAS: [
+        { id: "pet_slime", type: "MASCOTA", rarity: "SSR", name: "Slime Devorador", era: "UNIVERSAL", desc: "+30 ATK, +30 DEF." }
+    ],
+    OBJETOS: [
+        { id: "obj_senzu", type: "OBJETO", rarity: "R", name: "Semilla del Ermitaño", era: "UNIVERSAL", desc: "+40 HP." }
+    ],
+    HABILIDADES: [
+        { id: "skill_arise", type: "HABILIDAD", rarity: "SSR", name: "[Extracción de Sombras]", era: "UNIVERSAL", desc: "+40 ATK." }
+    ],
+    TALENTOS: [
+        { id: "t_n1", type: "TALENTO", rarity: "N", name: "Cuerpo Resistente", era: "UNIVERSAL", desc: "+10 HP." },
+        { id: "t_r1", type: "TALENTO", rarity: "R", name: "Instinto Asesino", era: "UNIVERSAL", desc: "+15 ATK en emboscadas." }
+    ]
+};
